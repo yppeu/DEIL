9c1009cbd2f09ac8f6943cb60845836ab006ec2e
sua yeon <yppeu@naver.com>
2023-01-17 19:11:59 +0900

    0117

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,20 @@
 $(function () {
-  $(".myslider").slick({
-    Infinity: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    fade: false,
-    cssEase: "linear",
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  var swiper = new Swiper(".mainSwiper", {
+    effect: "cube",
+    grabCursor: true,
+    slidesPerView: "1", //화면에 여러개 패널 보이기
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+  });
+  var swiper = new Swiper(".bestSwiper", {
+    watchSlidesProgress: true,
+    slidesPerView: 1,
   });
 });
