0cdf0628e279900c89b0cf551f10e6de9a570ba2
sua yeon <yppeu@naver.com>
2023-01-19 15:32:10 +0900

    0119

--- js/slider.js
+++ js/slider.js
@@ -37,3 +37,31 @@ $(function () {
     mousewheel: true,
   });
 });
+
+// 더보기 버튼
+$(function () {
+  let test = $("#more_list>li").slice(0, 1).show(); // 초기갯수
+  $("#more_list>li").addClass("li_bg");
+  $(".li_bg").css({
+    background: "rgba(0, 0, 0, 0.1)",
+    "border-radius": "20px",
+    "line-height": "25px",
+    "text-align": "left",
+    margin: "3px",
+    "padding-top": "6px",
+    "padding-left": "6px",
+  });
+  $("#more_list>li").removeClass("li_bg");
+  console.log(test);
+  $(".more")
+    .css("cursor", "pointer")
+    .click(function (e) {
+      // 클릭시 more
+      // e.preventDefault();
+      $("#more_list>li:hidden").slice(0, 3).show(); // 클릭시 more 갯수 지저정
+      if ($("#more_list>li:hidden").length < 0) {
+        // 컨텐츠 남아있는지 확인
+        alert("게시물의 끝입니다."); // 컨텐츠 없을시 alert 창 띄우기
+      }
+    });
+});
